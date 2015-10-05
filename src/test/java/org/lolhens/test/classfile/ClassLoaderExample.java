536938a9f0dc05739347b6876b7ca51d57ad496b
LolHens <pierrekisters@gmail.com>
2015-10-05 12:15:47 +0200

    added src files and build configuration
    migrated from LibEventManager

---
+++ src/test/java/org/lolhens/test/classfile/ClassLoaderExample.java
@@ -0,0 +1,33 @@
+package org.lolhens.test.classfile;
+
+import org.lolhens.classfile.URLClassLocation;
+
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class ClassLoaderExample {
+    public static void main(String[] args) {
+        Class<?> loadedClass;
+        try {
+            for (URLClassLocation ucl : new URLClassLocation(ClassLoaderExample.class).discoverSourceURL()) {
+                if (ucl.getQualifiedName().toLowerCase().startsWith("com.dafttech")) {
+                    loadedClass = ucl.loadClass(ClassLoaderExample.class.getClassLoader());
+                    if (!loadedClass.isInterface() && IRunnableModule.class.isAssignableFrom(loadedClass)) {
+                        try {
+                            Constructor<?> constructor = loadedClass.getDeclaredConstructor();
+                            constructor.setAccessible(true);
+                            Object newInstance = constructor.newInstance();
+                            IRunnableModule module = (IRunnableModule) newInstance;
+                            module.run();
+                        } catch (NoSuchMethodException | SecurityException | IllegalAccessException | InstantiationException | IllegalArgumentException | InvocationTargetException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            }
+        } catch (ClassNotFoundException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
